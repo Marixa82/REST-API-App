4cbdc6358bade0e705f8c4e41c689d896ead147f
Marixa82 <121823496+Marixa82@users.noreply.github.com>
2024-01-10 20:25:01 +0200

    hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,45 @@
-const express = require('express')
+// const express = require('express')
+import express from 'express';
+import * as contactsService from '../../models/contacts.js';
+import { HttpError } from '../../helpers/index.js';
 
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contactsService.listContacts()
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await contactsService.getContactById(contactId);
+    if (!result) {
+      throw HttpError(404, "Not found")
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contactsService.addContact(req.body);
+    if (!result) {
+      throw HttpError(400, "missing required name field")
+    }
+    res.status(201).json(result);
+
+  } catch (error) {
+    next(error);
+  }
+  // res.json({ message: 'template message' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
@@ -22,4 +50,4 @@ router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-module.exports = router
+export default router;
